f7ed5b5c455a92b327cfa39b614b343b8efe27f1
Ruslan <posuhovskiyruslan@gmail.com>
2023-11-26 11:35:04 +0200

    product

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+class ProductWork
+{
+    public function get ($name) {}
+    public function set ($name, $value) {}
+    public function save() {}
+
+}
+
+class ProductProcessing
+{
+    public function update() {}
+    public function delete() {}
+}
+
+class ProductPresent
+{
+    public function show () {}
+    public function print () {}
+}
